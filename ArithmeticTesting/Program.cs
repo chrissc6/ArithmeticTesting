bdce872698adf4cbaa120dfa024e3128a7c12ada
Chris Caudill <christopherscaudill@gmail.com>
2019-02-18 09:58:42 -0500

    Add project files.

---
+++ ArithmeticTesting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
